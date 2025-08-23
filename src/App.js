00ee9f98730d3aba12aa129c1b058a1b590c14b5
Lukáš Stika <unax@seznam.cz>
2025-08-23 11:26:06 +0200

    enviroment config

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.scss";
+
+import BudgetSummary from "./components/budgetSummary";
+import Form from "./components/form";
+import List from "./components/list";
+import "./App.scss";
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Budget Manager</h1>
+      <p>Ptest prvni verze</p>
+    </div>
+  );
+}
+
+export default App;
